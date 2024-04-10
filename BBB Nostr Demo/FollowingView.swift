857d0863eccbde938406d625d262f22912b11eb1
Bryan Montz <bryan.montz@lifechurch.tv>
2024-04-10 06:53:17 -0500

    FollowingView: simplifications and improvements

--- BBB Nostr Demo/FollowingView.swift
+++ BBB Nostr Demo/FollowingView.swift
@@ -15,25 +15,20 @@ struct FollowingView: View {
     @EnvironmentObject var users: Users
     
     @State private var events: [NostrEvent] = []
-    @State private var subscriptionId: String?
     @State private var eventsCancellable: AnyCancellable?
     
     var body: some View {
         VStack {
             Text("\(users.otherUser.name)'s notes")
             
-            List(events, id: \.id) { event in
-                if !event.content.isEmpty {
-                    Text("\(event.content)")
-                } else {
-                    Text("Empty content field for event \(event.id)")
-                }
+            List(events.sorted(by: { $0.createdDate > $1.createdDate }), id: \.id) { event in
+                Text("\(event.content)")
             }
         }
         .onAppear {
             let filter = Filter(authors: [users.otherUser.keypair.publicKey.hex],
                                 kinds: [EventKind.textNote.rawValue])
-            subscriptionId = relayPool.subscribe(with: filter)
+            _ = relayPool.subscribe(with: filter)
             
             eventsCancellable = relayPool.events
                 .receive(on: DispatchQueue.main)
